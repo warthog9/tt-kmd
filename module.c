33cf76165cf9f89da3dcb3eedfcaa26262051a7f
Andrew Lewycky <alewycky@tenstorrent.com>
2023-12-06 15:05:35 -0500

    Change version to 1.28-bh

--- module.c
+++ module.c
@@ -9,7 +9,7 @@
 #include "chardev.h"
 #include "enumerate.h"
 
-#define TTDRIVER_VER "1.28"
+#define TTDRIVER_VER "1.28-bh"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Tenstorrent AI kernel driver");
